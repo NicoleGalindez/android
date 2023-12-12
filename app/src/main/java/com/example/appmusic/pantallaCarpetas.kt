806027fd1ba543bcf3f63efbad0b365abcf42d2a
NicoleGalindez <nbgalindez@misena.edu.co>
2023-12-12 16:04:15 -0500

    elegir cada quien

---
+++ app/src/main/java/com/example/appmusic/pantallaCarpetas.kt
@@ -0,0 +1,11 @@
+package com.example.appmusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantallaCarpetas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_carpetas)
+    }
+}
