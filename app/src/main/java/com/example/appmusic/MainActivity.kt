3a786760beaf6a44e820b005dff265a44e94ddb9
NicoleGalindez <nbgalindez@misena.edu.co>
2023-12-12 14:29:45 -0500

    android

---
+++ app/src/main/java/com/example/appmusic/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.appmusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+}
+}
