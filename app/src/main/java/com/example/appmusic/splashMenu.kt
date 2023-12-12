400db5502df4ab18778e7a77baff59f6585a3829
NicoleGalindez <nbgalindez@misena.edu.co>
2023-12-12 16:04:15 -0500

    elegir cada quien

---
+++ app/src/main/java/com/example/appmusic/splashMenu.kt
@@ -0,0 +1,11 @@
+package com.example.appmusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_menu)
+    }
+}
